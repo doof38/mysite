61b2c0a7a3645141c3dbf9cf4d8ae95e059ee885
Pranav Jerry <twothousand41@gmail.com>
2023-06-09 22:44:13 +0530

    added update pip in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+pip install --upgrade pip
 pip install git+https://git.disroot.org/pranav/django-todoapi git+https://git.disroot.org/pranav/django-todo gunicorn
 
 python manage.py collectstatic --no-input
