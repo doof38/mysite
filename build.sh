0df5741a3674d1d30fe2f018e4bab2b10fdf5424
Pranav Jerry <twothousand41@gmail.com>
2023-06-09 22:48:03 +0530

    changed build.sh again

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # exit on error
 set -o errexit
 pip install --upgrade pip
-pip install --upgrade python
+#pip install --upgrade python
 # update all packages
 pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))"
 pip install git+https://git.disroot.org/pranav/django-todoapi git+https://git.disroot.org/pranav/django-todo gunicorn
