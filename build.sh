87a6cdbdde3265c2281b623fcb55214fbed6d1c0
Pranav Jerry <twothousand41@gmail.com>
2023-06-10 11:03:49 +0530

    changed build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,11 @@ pip install --upgrade pip
 pip --disable-pip-version-check list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))"
 pip install git+https://git.disroot.org/pranav/django-todoapi git+https://git.disroot.org/pranav/django-todo gunicorn whitenoise[brotli]
 
+django-admin startproject newsite
+cd newsite/newsite
+cat settings.py
+echo 'INSTALLED_APPS+=["todo","todoapi]' >>settings.py
+cd ..
+
 python manage.py collectstatic --no-input
 python manage.py migrate
